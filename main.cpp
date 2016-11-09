46eea2d8dd8dcc324fb81d5f2b6cfc59bc23ed1b
Steve Biedermann <steve.biedermann.privat@gmail.com>
2016-11-09 08:34:18 +0100

    added main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <iostream>
+
+#include <string>
+using std::string;
+using std::cout;
+using std::endl;
+
+int main( int argc, const char* argv[] )
+{
+	cout << "Hello World!" << endl;
+}
